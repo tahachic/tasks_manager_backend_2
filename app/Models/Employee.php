19dd473e85d61ca307084663e938017188a8e764
tahachic <azetaha@gmail.com>
2025-03-12 12:49:43 +0100

    hgfghgf

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Employee extends Authenticatable
 {
-    use HasApiTokens, HasFactory;
+    use HasApiTokens, HasFactory, SoftDeletes;
 
     protected $fillable = ['name', 'password', 'department_id', 'account_type'];
 
